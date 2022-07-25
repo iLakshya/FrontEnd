3ecb1ea6f08b21d7d0be9085394ed89e4499eaba
HaripriyaJ <haripriyajagandr@gmail.com>
2022-07-25 23:14:19 +0530

    JS discussion summary list

---
+++ sample1.js
@@ -0,0 +1,35 @@
+// Data types
+
+// Number
+// String
+// Boolean
+// Arrays
+// Objects
+// Sets
+
+// Variables
+
+// var
+// const
+// let
+
+// print using console.log
+// run a js file
+
+// Operators
+
+// AND (&&)
+// OR (||)
+// NOT (!)
+
+// Conditional statements
+
+// if
+// if...else
+// if...else if...else
+// switch
+
+// Control statements
+
+// return
+// break
